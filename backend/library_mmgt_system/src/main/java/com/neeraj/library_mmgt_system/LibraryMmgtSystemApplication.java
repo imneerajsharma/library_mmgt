c3267fd5c265947e20da4227e2112cf58a1156d7
Neeraj Sharma <neerajkumarsharma279@gmail.com>
2024-10-10 15:07:18 -0400

    first commit

---
+++ backend/library_mmgt_system/src/main/java/com/neeraj/library_mmgt_system/LibraryMmgtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.neeraj.library_mmgt_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMmgtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryMmgtSystemApplication.class, args);
+	}
+
+}
